4e3d9ac80d6b92041a74beb00fc35feab2b34c6a
weswil07 <weswil07@gmail.com>
2016-03-23 00:16:21 +0000

    add mongo lab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
     
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://wes:southgwinnett@ds021969.mlab.com:21969/yelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
